7711fb1b985b213a84fe4cf0051714cc008a100f
OtenMoten <waterandwind@pm.me>
2022-02-05 22:34:38 +0100

    re-formatting and basing

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,6 @@ COPY package*.json ./
 RUN npm config set "@fortawesome:registry" https://npm.fontawesome.com/
 RUN npm config set "//npm.fontawesome.com/:_authToken" 18E48DC6-61F5-4717-BC5D-4A1155A99FA4
 RUN npm install
-#RUN npm install ejs
-#RUN npm install express
-#RUN npm install ejs-express-layouts
-#RUN npm install fontawesome
 COPY . .
 EXPOSE 8080
 CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const app = express();
 const path = require("path");
 const https = require("https");
 const fs = require("fs");
+const mongoose = require("mongoose");
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/css", express.static(path.join(__dirname, "node_modules/mdb-ui-kit/css")));
-app.use("/js", express.static(path.join(__dirname, "node_modules/mdb-ui-kit/js")));
+// app.use("/css", express.static(path.join(__dirname, "node_modules/mdb-ui-kit/css")));
+// app.use("/js", express.static(path.join(__dirname, "node_modules/mdb-ui-kit/js")));
 app.use("/js/all.js", express.static(path.join(__dirname, "node_modules/@fortawesome/fontawesome-pro/js/all.js")));
 
 app.set("view engine", "ejs");
@@ -17,23 +18,40 @@ app.use(expressLayouts);
 app.set("layout", "layouts/primary");
 
 app.get("/", (req, res) => {
-    res.render("home", { title: "Startseite"});
+    res.render("landing/home", {title: "Startseite"});
 });
 
-app.get("/kochen", (req, res) => {
-    res.render("cooking/cooking", { layout: "layouts/secondary", title: "Kochen"});
+app.get("/cannabis-kochen", (req, res) => {
+    res.render("cooking/cooking", {layout: "layouts/secondary", title: "Mahlzeiten & Desserts"});
 });
 
-https
-    .createServer(
-        // Provide the private and public key to the server by reading each
-        // file's content with the readFileSync() method.
-        {
-            key: fs.readFileSync("key.pem"),
-            cert: fs.readFileSync("cert.pem"),
-        },
-        app
-    )
-    .listen(8080, "0.0.0.0",() => {
-        console.log("serever is runing at port 8080");
+app.get("/cannabis-drinks", (req, res) => {
+    res.render("drinks/drinks", {layout: "layouts/secondary", title: "Drinks & Cocktails"});
 });
+
+const options = {
+    user: "admin",
+    pass: "0bZ*5eOz!bx3",
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+mongoose.connect("mongodb://db.four20.green/four20cooking?authSource=admin", options, () => {
+    console.log("Successfully connected to four20cooking database *");
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection to four20cooking FAILED !!!"))
+db.once("open", () => {
+    console.log("Initialize mongoose ...")
+});
+
+https.createServer(
+    // Provide the private and public key to the server by reading each
+    // file's content with the readFileSync() method.
+    {
+        key: fs.readFileSync("key.pem"),
+        cert: fs.readFileSync("cert.pem"),
+    }, app)
+    .listen(7070, "0.0.0.0", () => {
+        console.log("Server listen on port 7070 *");
+    });
